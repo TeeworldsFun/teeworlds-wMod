720ff138ae3eb7673647eaa2a86b2559dbb3ea59
PJK136 <du.paul@hotmail.fr>
2011-09-06 16:24:50 +0200

    Add the way alea of loot

--- src/game/server/gamecontroller.cpp
+++ src/game/server/gamecontroller.cpp
@@ -440,19 +440,16 @@ int IGameController::OnCharacterDeath(class CCharacter *pVictim, class CPlayer *
 
 	CLoot *loot = new CLoot(&GameServer()->m_World, POWERUP_HEALTH, 0);
 	loot->m_Pos = pVictim->m_Pos;
-	loot->m_Pos.x -= 50;
-	loot->m_Pos.y -= 50;
+	while (GameServer()->Collision()->CheckPoint(loot->m_Pos.x -= (rand() % (100 - 0 + 1)) + 0, loot->m_Pos.y -= (rand() % (100 - 0 + 1)) + 0));
 
 	loot = new CLoot(&GameServer()->m_World, POWERUP_ARMOR, 0);
 	loot->m_Pos = pVictim->m_Pos;
-	loot->m_Pos.x += 50;
-	loot->m_Pos.y -= 50;
+	while (GameServer()->Collision()->CheckPoint(loot->m_Pos.x -= (rand() % (100 - 0 + 1)) + 0, loot->m_Pos.y -= (rand() % (100 - 0 + 1)) + 0));
 
 	if ( pVictim->GetActiveWeapon() != WEAPON_GUN && pVictim->GetActiveWeapon() != WEAPON_HAMMER )
 	{
 		loot = new CLoot(&GameServer()->m_World, POWERUP_WEAPON, pVictim->GetActiveWeapon());
-		loot->m_Pos = pVictim->m_Pos;
-		loot->m_Pos.y -= 50;
+		while (GameServer()->Collision()->CheckPoint(loot->m_Pos.x -= (rand() % (100 - 0 + 1)) + 0, loot->m_Pos.y -= (rand() % (100 - 0 + 1)) + 0));
 	}
 
 	return 0;
