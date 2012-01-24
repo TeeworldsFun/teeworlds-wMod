6d2d75082cdee861522667a0d938cc5568081cc6
PJK136 <du.paul@hotmail.fr>
2012-01-24 18:01:43 +0100

    Fixed lag due to mines

--- src/game/server/entities/projectile.cpp
+++ src/game/server/entities/projectile.cpp
@@ -72,60 +72,103 @@ void CProjectile::Tick()
     float Ct = (Server()->Tick()-m_StartTick)/(float)Server()->TickSpeed();
     vec2 PrevPos = GetPos(Pt);
     vec2 CurPos = GetPos(Ct);
+    m_LifeSpan--;
 
-    vec2 At;
-    CTeleporter *CollideTeleporter = (CTeleporter *)GameServer()->m_World.IntersectEntity(PrevPos, CurPos, 24.0f, At, CGameWorld::ENTTYPE_TELEPORTER);
-    if (CollideTeleporter && CollideTeleporter->GetNext())
-    {
-        m_Direction = normalize(GetPos(Ct) - PrevPos);
-        m_Pos = CollideTeleporter->GetNext()->m_Pos + (m_Direction * (50.0f));
-        m_StartTick = Server()->Tick();
-        Pt = (Server()->Tick()-m_StartTick-1)/(float)Server()->TickSpeed();
-        Ct = (Server()->Tick()-m_StartTick)/(float)Server()->TickSpeed();
-        PrevPos = GetPos(Pt);
-        CurPos = GetPos(Ct);
-    }
-
-    int Collide = distance(CurPos, PrevPos) ? GameServer()->Collision()->IntersectLine(PrevPos, CurPos, &CurPos, 0) : GameServer()->Collision()->CheckPoint(CurPos);
     CCharacter *OwnerChar = GameServer()->GetPlayerChar(m_Owner);
-    CCharacter *TargetChr = GameServer()->m_World.IntersectCharacter(PrevPos, CurPos, 6.0f, CurPos, OwnerChar);
-    CTurret *TargetTurret = (CTurret *)GameServer()->m_World.IntersectEntity(PrevPos, CurPos, 6.0f, CurPos, CGameWorld::ENTTYPE_TURRET);
-    CExplodeWall *TargetExplodeWall = 0;
+    CCharacter *TargetChr = 0;
+    int Collide = false;
 
+    if (distance(CurPos, PrevPos))
     {
-        CExplodeWall *p = (CExplodeWall *)GameWorld()->FindFirst(CGameWorld::ENTTYPE_EXPLODEWALL);
-        for(; p; p = (CExplodeWall *)p->TypeNext())
+        vec2 At;
+        CTeleporter *CollideTeleporter = (CTeleporter *)GameServer()->m_World.IntersectEntity(PrevPos, CurPos, 24.0f, At, CGameWorld::ENTTYPE_TELEPORTER);
+        if (CollideTeleporter && CollideTeleporter->GetNext())
         {
-            // Store the values for fast access and easy
-            // equations-to-code conversion
-            float x1 = PrevPos.x, x2 = CurPos.x, x3 = p->m_From.x, x4 = p->m_Pos.x;
-            float y1 = PrevPos.y, y2 = CurPos.y, y3 = p->m_From.y, y4 = p->m_Pos.y;
-
-            float d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-            // If d is zero, there is no intersection
-            if (d == 0)
-                continue;
-
-            // Get the x and y
-            float pre = (x1*y2 - y1*x2), post = (x3*y4 - y3*x4);
-            float x = ( pre * (x3 - x4) - (x1 - x2) * post ) / d;
-            float y = ( pre * (y3 - y4) - (y1 - y2) * post ) / d;
-
-            // Check if the x and y coordinates are within both lines
-            if ( x < min(x1, x2) || x > max(x1, x2) || x < min(x3, x4) || x > max(x3, x4) )
-                continue;
-            if ( y < min(y1, y2) || y > max(y1, y2) || y < min(y3, y4) || y > max(y3, y4) )
-                continue;
-
-            CurPos.x = x;
-            CurPos.y = y;
-            TargetExplodeWall = p;
-            break;
+            m_Direction = normalize(GetPos(Ct) - PrevPos);
+            m_Pos = CollideTeleporter->GetNext()->m_Pos + (m_Direction * (50.0f));
+            m_StartTick = Server()->Tick();
+            Pt = (Server()->Tick()-m_StartTick-1)/(float)Server()->TickSpeed();
+            Ct = (Server()->Tick()-m_StartTick)/(float)Server()->TickSpeed();
+            PrevPos = GetPos(Pt);
+            CurPos = GetPos(Ct);
         }
-    }
 
-    if ( !distance(CurPos, PrevPos) && !TargetChr)
+        Collide = GameServer()->Collision()->IntersectLine(PrevPos, CurPos, &CurPos, 0);
+        TargetChr = GameServer()->m_World.IntersectCharacter(PrevPos, CurPos, 6.0f, CurPos, OwnerChar);
+        CTurret *TargetTurret = (CTurret *)GameServer()->m_World.IntersectEntity(PrevPos, CurPos, 6.0f, CurPos, CGameWorld::ENTTYPE_TURRET);
+        CExplodeWall *TargetExplodeWall = 0;
+
+        {
+            CExplodeWall *p = (CExplodeWall *)GameWorld()->FindFirst(CGameWorld::ENTTYPE_EXPLODEWALL);
+            for(; p; p = (CExplodeWall *)p->TypeNext())
+            {
+                // Store the values for fast access and easy
+                // equations-to-code conversion
+                float x1 = PrevPos.x, x2 = CurPos.x, x3 = p->m_From.x, x4 = p->m_Pos.x;
+                float y1 = PrevPos.y, y2 = CurPos.y, y3 = p->m_From.y, y4 = p->m_Pos.y;
+
+                float d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+                // If d is zero, there is no intersection
+                if (d == 0)
+                    continue;
+
+                // Get the x and y
+                float pre = (x1*y2 - y1*x2), post = (x3*y4 - y3*x4);
+                float x = ( pre * (x3 - x4) - (x1 - x2) * post ) / d;
+                float y = ( pre * (y3 - y4) - (y1 - y2) * post ) / d;
+
+                // Check if the x and y coordinates are within both lines
+                if ( x < min(x1, x2) || x > max(x1, x2) || x < min(x3, x4) || x > max(x3, x4) )
+                    continue;
+                if ( y < min(y1, y2) || y > max(y1, y2) || y < min(y3, y4) || y > max(y3, y4) )
+                    continue;
+
+                CurPos.x = x;
+                CurPos.y = y;
+                TargetExplodeWall = p;
+                break;
+            }
+        }
+        
+        if ( m_Deploy && m_Type == WEAPON_SHOTGUN && (!Collide || GameServer()->m_pEventsGame->IsActualEvent(BULLET_PIERCING)) && m_LifeSpan < 0 )
+        {
+            int ShotSpread = 2;
+
+            CMsgPacker Msg(NETMSGTYPE_SV_EXTRAPROJECTILE);
+            Msg.AddInt(ShotSpread*2+1);
+
+            for(int i = -ShotSpread; i <= ShotSpread; ++i)
+            {
+                float Spreading[] = {-0.185f, -0.070f, 0, 0.070f, 0.185f};
+                float a = GetAngle(m_Direction);
+                a += Spreading[i+2];
+                float v = 1-(absolute(i)/(float)ShotSpread);
+                float Speed = mix((float)GameServer()->Tuning()->m_ShotgunSpeeddiff, 1.0f, v);
+
+                CProjectile *pProj = new CProjectile(GameWorld(), m_Weapon,
+                                                     m_Owner,
+                                                     CurPos,
+                                                     vec2(cosf(a), sinf(a))*Speed,
+                                                     (int)(Server()->TickSpeed()*GameServer()->Tuning()->m_ShotgunLifetime),
+                                                     1, m_Explosive, 0, m_SoundImpact, m_Weapon, m_Limit, m_Smoke);
+
+                // pack the Projectile and send it to the client Directly
+                CNetObj_Projectile p;
+                pProj->FillInfo(&p);
+
+                for(unsigned i = 0; i < sizeof(CNetObj_Projectile)/sizeof(int); i++)
+                    Msg.AddInt(((int *)&p)[i]);
+            }
+
+            Server()->SendMsg(&Msg, 0, m_Owner);
+        }
+        
+        if (m_Smoke && !Collide && (!GameServer()->m_pEventsGame->IsActualEvent(BULLET_PIERCING) || GameServer()->Collision()->CheckPoint(PrevPos) == false) && (Server()->Tick()-m_StartTick) % 2 == 0 )
+            GameServer()->CreateExplosion(CurPos, m_Owner, m_Weapon, false, true);
+    }
+    else if ((Server()->Tick()-m_StartTick) % 2 == 0)
     {
+        Collide = GameServer()->Collision()->CheckPoint(CurPos);
         CCharacter *apEnts[MAX_CLIENTS] = {0};
         int Num = GameServer()->m_World.FindEntities(CurPos, 6.0f, (CEntity**)apEnts, MAX_CLIENTS, CGameWorld::ENTTYPE_CHARACTER);
         for(int i = 0; i < Num; i++)
@@ -136,44 +179,9 @@ void CProjectile::Tick()
                 break;
             }
         }
-    }
-
-    if (m_Smoke && (Server()->Tick()-m_StartTick) % 2 == 0 && (!GameServer()->m_pEventsGame->IsActualEvent(BULLET_PIERCING) || GameServer()->Collision()->CheckPoint(PrevPos) == false) && !Collide)
-        GameServer()->CreateExplosion(CurPos, m_Owner, m_Weapon, false, true);
-
-    m_LifeSpan--;
-
-    if ( m_Deploy && m_Type == WEAPON_SHOTGUN && (!Collide || GameServer()->m_pEventsGame->IsActualEvent(BULLET_PIERCING)) && m_LifeSpan < 0 )
-    {
-        int ShotSpread = 2;
-
-        CMsgPacker Msg(NETMSGTYPE_SV_EXTRAPROJECTILE);
-        Msg.AddInt(ShotSpread*2+1);
-
-        for(int i = -ShotSpread; i <= ShotSpread; ++i)
-        {
-            float Spreading[] = {-0.185f, -0.070f, 0, 0.070f, 0.185f};
-            float a = GetAngle(m_Direction);
-            a += Spreading[i+2];
-            float v = 1-(absolute(i)/(float)ShotSpread);
-            float Speed = mix((float)GameServer()->Tuning()->m_ShotgunSpeeddiff, 1.0f, v);
-
-            CProjectile *pProj = new CProjectile(GameWorld(), m_Weapon,
-                                                 m_Owner,
-                                                 CurPos,
-                                                 vec2(cosf(a), sinf(a))*Speed,
-                                                 (int)(Server()->TickSpeed()*GameServer()->Tuning()->m_ShotgunLifetime),
-                                                 1, m_Explosive, 0, m_SoundImpact, m_Weapon, m_Limit, m_Smoke);
-
-            // pack the Projectile and send it to the client Directly
-            CNetObj_Projectile p;
-            pProj->FillInfo(&p);
-
-            for(unsigned i = 0; i < sizeof(CNetObj_Projectile)/sizeof(int); i++)
-                Msg.AddInt(((int *)&p)[i]);
-        }
-
-        Server()->SendMsg(&Msg, 0, m_Owner);
+        
+        if (!Collide && m_Smoke)
+            GameServer()->CreateExplosion(CurPos, m_Owner, m_Weapon, false, true);
     }
 
     if(TargetChr || TargetTurret || TargetExplodeWall || Collide || m_LifeSpan < 0 || GameLayerClipped(CurPos))
